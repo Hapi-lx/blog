0c86facb268018f66006533d7382505711dbdb02
Hapi-lx <58059194+Hapi-lx@users.noreply.github.com>
2023-01-04 10:21:29 +0000

    Deploying to gh-pages from @ Hapi-lx/blog@abb16af3d6313b3162a4a3b27ea4ba5821721a46 🚀

---
+++ assets/index.html-a077e2a6.js
@@ -0,0 +1,6 @@
+import{_ as e,V as o,W as t,X as n}from"./framework-8edd236a.js";const s={};function a(r,l){return o(),t("div",null,[n(` 
+This is a blog home page demo.
+
+To use this layout, you should set both \`layout: BlogHome\` and \`home: true\` in the page front matter.
+
+For related configuration docs, please see [blog homepage](https://vuepress-theme-hope.github.io/v2/guide/blog/home/). `)])}const i=e(s,[["render",a],["__file","index.html.vue"]]);export{i as default};
